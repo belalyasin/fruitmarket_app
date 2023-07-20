627b84a4ea65299ca58304ee8fedea10b5a18227
belalyasin <belalabwhatb2@gmail.com>
2023-07-20 20:31:00 +0300

    fix :edit som changes on api/productController

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\NutritionResources;
 use App\Models\Nutrition;
 use App\Models\Product;
+use App\Models\Product_Nutrition;
 use App\Models\Sub_Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -17,7 +20,7 @@ class ProductController extends Controller
     public function index()
     {
         //
-        $products = Product::with(['sub__category', 'product_nutrition'])->get();
+        $products = Product::with(['category', 'product_nutrition'])->get();
         $data = $products->map(function ($product) {
             return [
                 "id" => $product->id,
@@ -26,8 +29,8 @@ class ProductController extends Controller
                 "rate" => $product->rate,
                 "description" => $product->description,
                 "image" => $product->image,
-                "isFavorite" => $product->isFavorite ? true : false,
-                "sub_categories" => $product->sub__category ? $product->sub__category->title : null,
+                "isFavorite" => (bool)$product->isFavorite,
+                "sub_category" => $product->category ? $product->category->title : null,
                 'nutrition' => NutritionResources::collection(
                     Nutrition::whereIn('id', $product->product_nutrition->pluck('nutrition_id'))->get()
                 ),
@@ -42,6 +45,50 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
+        $validation = Validator($request->all(), [
+            "name" => 'required|string|min:3',
+            "price" => 'required|double',
+            "rate" => 'required|integer',
+            "description" => 'required|string',
+            "image" => 'required|file|mimes:jpeg,png,jpg',
+            "subcategoryId" => 'required|numeric|exists:App\Models\Sub_Category,id',
+            "nutrients" => 'required|array',
+            "nutrients.*" => 'exists:App\Models\Nutrition,id',
+        ]);
+        if (!$validation->fails()) {
+            $product = new Product();
+            $product->name = $request->input('name');
+            $product->price = $request->input('price');
+            $product->rate = $request->input('rate');
+            $product->description = $request->input('description');
+            $product->category_id = $request->input("subcategoryId");
+            try{
+                if ($request->has('nutrients')) {
+                    $nutrients = $request->input('nutrients');
+                    foreach ($nutrients as $key => $value){
+                        Product_Nutrition::create([
+                            "product_id"=>$product->id,
+                            "nutrient_id"=>$value["nutrient"],
+                            "amountIn100g"=>(int)$value["amount"]*10
+                            ]);
+                    }
+                }
+                $path=$this->uploadImage($request);
+                $product->imageUrl="http://localhost/api/$path";
+                $product->saveOrFail();
+            }
+            catch(\Exception $e){
+                dd($e);
+                throw  new \Exception;
+            }
+        } else {
+            return response()->json(
+                [
+                    "message" => $validation->getMessageBag()->first()
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
     }
 
     /**
