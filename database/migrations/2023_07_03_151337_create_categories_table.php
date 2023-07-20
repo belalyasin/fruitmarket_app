a930ea97178c26ea32b73242e2d41b87972bbb05
belalyasin <belalabwhatb2@gmail.com>
2023-07-20 23:54:47 +0300

    feat : edit some column on category migration

--- database/migrations/2023_07_03_151337_create_categories_table.php
+++ database/migrations/2023_07_03_151337_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('description')->nullable();
             $table->float('discount')->nullable();
-            $table->foreignId('parent_id')->constrained('categories','id');
+            $table->foreignId('parent_id')->default(0)->constrained('categories', 'id')->cascadeOnUpdate()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
