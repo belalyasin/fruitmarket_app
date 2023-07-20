f8c6e8a865c6500f5236877ea5b4282f17848f57
belalyasin <belalabwhatb2@gmail.com>
2023-07-20 20:02:51 +0300

    feat : Add lang files ar , en

---
+++ resources/lang/ar/cms.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "app_name" => "Medical System",
+    "dashboard" => "Dashboard",
+    "content_management" => "Content Management",
+    "index" => "Index",
+    "create" => "Create",
+    "cities" => "Cities",
+    "edit" => "Edit",
+];
