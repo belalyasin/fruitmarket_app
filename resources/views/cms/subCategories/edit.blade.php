bac4d0ae2db9d2f47c93a499d05538c59b40e93b
belalyasin <belalabwhatb2@gmail.com>
2023-07-20 20:39:00 +0300

    feat : Add index ,edit and create page for the blade view for subCategories

---
+++ resources/views/cms/subCategories/edit.blade.php
@@ -0,0 +1,99 @@
+@extends('cms.parent')
+
+@section('title',__('cms.dashboard'))
+
+@section('styles')
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{asset('cms/plugins/select2/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('cms/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+@endsection
+
+@section('main-content')
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">{{__('cms.edit_sub_category')}}</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form id="create-form">
+                            @csrf
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="title">{{__('cms.title')}}</label>
+                                    <input type="text" class="form-control" id="title" name="title"
+                                           value="{{$subCategory->title}}"
+                                           placeholder="{{__('cms.title')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="discount">{{__('cms.discount')}}</label>
+                                    <input type="number" class="form-control" id="discount" name="discount"
+                                           value="{{$subCategory->discount}}"
+                                           placeholder="{{__('cms.discount')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">{{__('cms.description')}}</label>
+                                    <input type="text" class="form-control" id="description" name="description"
+                                           value="{{$subCategory->description}}"
+                                           placeholder="{{__('cms.description')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>{{__('cms.category')}}</label>
+                                    <select class="form-control cities" style="width: 100%;" id="parent_id">
+                                        @foreach ($categories as $category)
+                                            <option value="{{$category->id}}"
+                                                    @if($subCategory->parent_id == $category->id) selected @endif>{{$category->title}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="button" onclick="performStore()"
+                                        class="btn btn-primary">{{__('cms.update')}}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!--/.col (left) -->
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
+
+@section('scripts')
+    <script src="{{asset('cms/plugins/select2/js/select2.full.min.js')}}"></script>
+
+    <script>
+        function performStore() {
+            // alert('Perform Store Function');
+            // console.log('Perform Store - Function');
+            axios.post('/cms/admin/subCategories', {
+                title: document.getElementById('title').value,
+                discount: document.getElementById('discount').value,
+                description: document.getElementById('description').value,
+                parent_id: document.getElementById('parent_id').value,
+            })
+                .then(function (response) {
+                    console.log(response);
+                    toastr.success(response.data.message);
+                    document.getElementById('create-form').reset();
+                    window.location.href = '/cms/admin/subCategories';
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    toastr.error(error.response.data.message);
+                });
+        }
+    </script>
+@endsection
---
+++ resources/views/cms/subCategories/index.blade.php
@@ -0,0 +1,126 @@
+@extends('cms.parent')
+
+@section('title',__('cms.category'))
+
+@section('page_name',__('cms.index'))
+@section('main_page',__('cms.sub_category'))
+@section('small_page_name',__('cms.index'))
+
+@section('styles')
+
+@endsection
+
+@section('main-content')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">{{__('cms.sub_category')}}</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>Title</th>
+                                    <th>Discount</th>
+                                    <th>description</th>
+                                    <th>Category</th>
+                                    <th>Created At</th>
+                                    <th>Settings</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($subCategories as $subCategory)
+                                    <tr>
+                                        <td>{{$subCategory->id}}</td>
+                                        <td>{{$subCategory->title}}</td>
+                                        <td>{{$subCategory->discount}}</td>
+                                        <td>@if($subCategory->description!=null)
+                                                {{ $subCategory->description }}
+                                            @else
+                                                -
+                                            @endif</td>
+                                        <td>{{$subCategory->parentCategory->title}}</td>
+                                        <td>{{\Carbon\Carbon::parse($subCategory->created_at)->format('Y-m-d')}}</td>
+                                        <td>
+                                            <div class="btn-group">
+                                                <a href="{{route('subCategories.edit',[$subCategory->id])}}"
+                                                   class="btn btn-warning">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <a href="#" onclick="confirmDelete('{{$subCategory->id}}',this)"
+                                                   class="btn btn-danger">
+                                                    <i class="fas fa-trash"></i>
+                                                </a>
+{{--                                                <form action="{{ route('subCategories.destroy', [$subCategory->id]) }}"--}}
+{{--                                                      method="POST">--}}
+{{--                                                    @csrf--}}
+{{--                                                    @method('DELETE')--}}
+{{--                                                    <a href="#" onclick="confirmDelete('{{$subCategory->id}}',this)"--}}
+{{--                                                       class="btn btn-danger">--}}
+{{--                                                        <i class="fas fa-trash"></i>--}}
+{{--                                                    </a>--}}
+{{--                                                        <button type="submit" class="btn btn-md btn-danger show-alert-delete-box"--}}
+{{--                                                                data-toggle="tooltip" title='Delete'><i class="fas fa-trash"></i></button>--}}
+{{--                                                </form>--}}
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+
+                        </div>
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+@endsection
+
+@section('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        function confirmDelete(id, element) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    performDelete(id, element)
+                }
+            })
+        }
+
+        function performDelete(id, element) {
+            // window.alert("here");
+            axios.delete("/cms/admin/subCategories/"+id)
+                .then(function (response) {
+                    console.log(response);
+                    toastr.success(response.data.message);
+                    element.closest('tr').remove();
+                })
+                .catch(function (error) {
+                    //4xx, 5xx
+                    console.log(error);
+                    toastr.error(error.response.data.message);
+                });
+        }
+    </script>
+{{--    @include('cms.alertScript')--}}
+@endsection
