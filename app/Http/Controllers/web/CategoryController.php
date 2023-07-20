69fff6e90ef266da92848d14867e46e3b1b22e45
belalyasin <belalabwhatb2@gmail.com>
2023-07-20 20:32:29 +0300

    feat : Add web/CategoryController

---
+++ app/Http/Controllers/web/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $categories = Category::where('parent_id','=','0')->get();
+        return response()->view('cms.categories.index',['categories'=> $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return response()->view('cms.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|min:3',
+            'description' => 'nullable|string|min:10'
+        ]);
+        if (!$validator->fails()) {
+            $category = new Category();
+            $category->title = $request->input('title');
+            $category->description = $request->input('description');
+            $category->parent_id = 0;
+            $isSaved = $category->save();
+            return response()->json(
+                ['message' => $isSaved ? 'Saved successfully' : 'Save failed'],
+                $isSaved ? Response::HTTP_CREATED : Response::HTTP_BAD_REQUEST
+            );
+        } else {
+            return response()->json(['message' => $validator->getMessageBag()->first()], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+        $category->delete();
+        return redirect()->back();
+    }
+}
