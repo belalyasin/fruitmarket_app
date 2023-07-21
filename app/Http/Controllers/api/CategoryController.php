5143ecd9a0e16f0bcc87cc84bd2eddaf0f2b27b5
belalyasin <belalabwhatb2@gmail.com>
2023-07-21 23:05:51 +0300

    fix :edit some changes query in category Controller -api

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        $categories = Category::where('parent_id','=','0')->get();
+        $categories = Category::where('parent_id','=',null)->get();
         $data = $categories->map(function ($category) {
             return [
                 'id' => $category->id,
