a71d90adcc0f1d9def53b3876951a5bac2e06069
belalyasin <belalabwhatb2@gmail.com>
2023-07-21 23:09:16 +0300

    fix :edit product Controller remove code in store and add code in show function for  productController-api

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -45,58 +45,29 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
-        $validation = Validator($request->all(), [
-            "name" => 'required|string|min:3',
-            "price" => 'required|double',
-            "rate" => 'required|integer',
-            "description" => 'required|string',
-            "image" => 'required|file|mimes:jpeg,png,jpg',
-            "subcategoryId" => 'required|numeric|exists:App\Models\Sub_Category,id',
-            "nutrients" => 'required|array',
-            "nutrients.*" => 'exists:App\Models\Nutrition,id',
-        ]);
-        if (!$validation->fails()) {
-            $product = new Product();
-            $product->name = $request->input('name');
-            $product->price = $request->input('price');
-            $product->rate = $request->input('rate');
-            $product->description = $request->input('description');
-            $product->category_id = $request->input("subcategoryId");
-            try{
-                if ($request->has('nutrients')) {
-                    $nutrients = $request->input('nutrients');
-                    foreach ($nutrients as $key => $value){
-                        Product_Nutrition::create([
-                            "product_id"=>$product->id,
-                            "nutrient_id"=>$value["nutrient"],
-                            "amountIn100g"=>(int)$value["amount"]*10
-                            ]);
-                    }
-                }
-                $path=$this->uploadImage($request);
-                $product->imageUrl="http://localhost/api/$path";
-                $product->saveOrFail();
-            }
-            catch(\Exception $e){
-                dd($e);
-                throw  new \Exception;
-            }
-        } else {
-            return response()->json(
-                [
-                    "message" => $validation->getMessageBag()->first()
-                ],
-                Response::HTTP_BAD_REQUEST
-            );
-        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
         //
+        $data = [
+            "id" => $product->id,
+            "name" => $product->name,
+            "price" => $product->price,
+            "rate" => $product->rate,
+            "description" => $product->description,
+            "image" => $product->image,
+            "isFavorite" => (bool)$product->isFavorite,
+            "sub_category" => $product->category ? $product->category->title : null,
+            'nutrition' => NutritionResources::collection(
+                Nutrition::whereIn('id', $product->product_nutrition->pluck('nutrition_id'))->get()
+            ),
+        ];
+
+        return response()->json([$data], status: 200);
     }
 
     /**
