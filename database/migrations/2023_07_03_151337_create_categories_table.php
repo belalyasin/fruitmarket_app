e7509c5588ea7081414208d6e5d3935e3051dc21
belalyasin <belalabwhatb2@gmail.com>
2023-07-21 23:12:44 +0300

    fix :edit some changes in migration Category

--- database/migrations/2023_07_03_151337_create_categories_table.php
+++ database/migrations/2023_07_03_151337_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->string('title');
             $table->string('description')->nullable();
             $table->float('discount')->nullable();
-            $table->foreignId('parent_id')->default(0)->constrained('categories', 'id')->cascadeOnUpdate()->cascadeOnUpdate();
+            $table->foreignId('parent_id')->constrained('categories', 'id')->cascadeOnUpdate()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
