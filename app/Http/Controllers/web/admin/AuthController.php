6f6dc59df5b0b530abdd3cfb9c16cc2ea2b89cb9
belalyasin <belalabwhatb2@gmail.com>
2023-08-16 22:29:17 +0300

    feat : Add to controller function to handel the route and render views

--- app/Http/Controllers/web/admin/AuthController.php
+++ app/Http/Controllers/web/admin/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\web\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
+use App\Models\Category;
+use App\Models\Nutrition;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -12,8 +15,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+    private $products, $categories, $subCategories, $nutritions, $users;
+
     /**
-     * Display a listing of the resource.
+     * Display a Login View.
      */
     public function loginView(Request $request)
     {
@@ -28,6 +33,7 @@ class AuthController extends Controller
             abort(Response::HTTP_NOT_FOUND, 'The page you have requested is not found');
         }
     }
+
     public function login(Request $request)
     {
         //
@@ -63,12 +69,34 @@ class AuthController extends Controller
         return redirect()->route('auth.login');
     }
 
+
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function welcom()
     {
         //
+        $this->categories = count(Category::where('parent_id', '=', null)->get('title'));
+        $this->subCategories = count(Category::where('parent_id', '!=', null)->get('title'));
+        $this->products = count(Product::get('name'));
+        $this->nutritions = count(Nutrition::get('name'));
+
+        return response()->view('cms.dashboard', [
+            'categories' => $this->categories,
+            'subCategories' => $this->subCategories,
+            'products' => $this->products,
+            'nutritions' => $this->nutritions,
+        ]);
+
+    }
+
+    public function show_profile()
+    {
+        //
+
+
+        return response()->view('cms.auth.admin.admin_profile');
+
     }
 
     /**
@@ -90,17 +118,44 @@ class AuthController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit_profile(Admin $admin)
     {
         //
+        return response()->view('cms.auth.admin.edit', ['admin' => $admin]);
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Admin $admin)
     {
         //
+//        dd($request);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3',
+            'email' => 'required|email|unique:admins,email,' . $admin->id,
+            "profile_image" => 'nullable|file|mimes:jpeg,png,jpg',
+        ]);
+        if (!$validator->fails()) {
+            $admin->name = $request->input('name');
+            $admin->email = $request->input('email');
+            if ($request->hasFile('profile_image')) {
+                $image = $request->file('profile_image');
+                // Save the image path to the admin
+                $path = time() . '.' . $image->getClientOriginalName();
+                $image->move(public_path('images'), $path);
+                $admin->profile_image = $path;
+            }
+            $isSaved = $admin->save();
+             response()->json(
+                ['message' => $isSaved ? 'Updated successfully' : 'Update failed'],
+                $isSaved ? Response::HTTP_CREATED : Response::HTTP_BAD_REQUEST
+            );
+            return redirect()->route('admin.admin_profile');
+        } else {
+            return response()->json(['message' => $validator->getMessageBag()->first()], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
