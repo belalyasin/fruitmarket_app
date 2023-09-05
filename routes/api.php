7f0da15027c88f942362485c1908139cc0a55d48
belalyasin <belalabwhatb2@gmail.com>
2023-09-06 01:40:34 +0300

    feat : Add new api route in api file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\api\Auth\AuthController;
+use App\Http\Controllers\api\CartController;
 use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\FavouriteController;
 use App\Http\Controllers\api\NutritionController;
+use App\Http\Controllers\api\OrderController;
 use App\Http\Controllers\api\ProductController;
 use App\Http\Controllers\api\SubCategoryController;
 use Illuminate\Http\Request;
@@ -21,17 +25,40 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 
-Route::prefix('category')->group(function () {
-    Route::get('/', [CategoryController::class, 'index']);
-});
-Route::prefix('sub-category')->group(function () {
-    Route::get('/', [SubCategoryController::class, 'index']);
-});
-Route::prefix('product')->group(function () {
-    Route::get('/', [ProductController::class, 'index']);
-    Route::get('/{product}', [ProductController::class, 'show']);
-});
-Route::prefix('nutrition')->group(function () {
-    Route::get('/', [NutritionController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/store-user-data', [AuthController::class, 'store']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/current-user', [AuthController::class, 'getCurrentUser']);
+    Route::PUT('/update-profile', [AuthController::class, 'updateProfile']);
+    Route::prefix('category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index']);
+    });
+    Route::prefix('sub-category')->group(function () {
+        Route::get('/', [SubCategoryController::class, 'index']);
+    });
+    Route::prefix('product')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/{product}', [ProductController::class, 'show']);
+        Route::post('/rate/{product}', [ProductController::class, 'rateProduct']);
+        Route::put('/{product}/favorite', [ProductController::class, 'update']);
+    });
+    Route::prefix('nutrition')->group(function () {
+        Route::get('/', [NutritionController::class, 'index']);
+    });
+    Route::prefix('favorite')->group(function () {
+        Route::get('/', [FavouriteController::class, 'index']);
+    });
+    Route::prefix('cart')->group(function () {
+        Route::get('/', [CartController::class, 'index']);
+        Route::post('/add-to-cart/{favoriteId}', [CartController::class, 'addToCart']);
+        Route::delete('/{cart}', [CartController::class, 'destroy']);
+    });
+    Route::prefix('order')->group(function () {
+        Route::get('/', [OrderController::class, 'index']);
+        Route::post('/place-order', [OrderController::class, 'placeOrder']);
+    });
 });
