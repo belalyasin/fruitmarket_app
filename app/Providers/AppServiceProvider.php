128c632b3d6c6333c688bf56e7428251dcc54e33
belalyasin <belalabwhatb2@gmail.com>
2023-09-06 13:42:20 +0300

    feat : Add some config and edit some changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if(App::environment('production')){
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
         //
         // Schema::defaultStringLength(191);
